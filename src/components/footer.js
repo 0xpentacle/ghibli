e69f8b29d531b87f9d977b627dbadc0f55d5bd79
“c5hex” <c5hex@protonmail.ch>
2021-05-12 13:39:46 +1200

    bits

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,19 +1,13 @@
 import React, {Component} from "react"
-import {Link} from "react-router-dom";
 
 class Footer extends Component {
     render() {
         return <footer>
             <article className={"footer-content flex-group space-between flex-align-center"}>
-                <div>© {new Date().getFullYear()} Pentacle
-                    <a href={"https://github.com/pentacleai"} target={"_blank"} rel="noopener noreferrer">GitHub</a>
-                    <a href={"https://pentacle.substack.com/"} target={"_blank"} rel="noopener noreferrer">Substack</a>
-                    <a href={"https://twitter.com/pentacleai"} target={"_blank"} rel="noopener noreferrer">Twitter</a>
+                <div>© 2000 - {new Date().getFullYear()} Studio Ghibli - this is a fan site. All images have been sourced from the public domain via google dot com.
+                    <p>Built for Ghibli enjoyers by <a href={"https://twitter.com/0xpentacle"} target={"_blank"} rel="noopener noreferrer">0xPentacle</a></p>
                 </div>
-                <div>
-                    <Link to="/terms">Terms</Link>
-                    <a href={"https://etherscan.io/address/pentacle.eth"} target={"_blank"} rel="noopener noreferrer">Donate pentacle.eth</a>
-                </div>
+
             </article>
         </footer>
     }
