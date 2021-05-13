44f64b0187b3b4bc14a091fe643f2ccffca4584c
“c5hex” <c5hex@protonmail.ch>
2021-05-13 16:52:44 +1200

    mobile oops

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ class Header extends Component {
     render() {
         return <header>
             <div className="header-content flex-justify-center align-center">
-                <h2>Studio Ghibli gif enjoyment</h2>
+                <h2>Ghibli gifs for gif enjoyers</h2>
             </div>
         </header>
     }
